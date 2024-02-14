930975af17eb1b8c3697c6bd1c3fdd5be8823a55
CoffCoder <58339090+CoffCoder@users.noreply.github.com>
2024-02-14 14:13:53 +0800

    BuildApk()

--- Assets/Editor/BuildApk.cs
+++ Assets/Editor/BuildApk.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class BuildApk : MonoBehaviour
+using UnityEditor;
+using System.IO;
+using System;
+public class TestBuild
 {
-    // Start is called before the first frame update
-    void Start()
+    [MenuItem("Build/BuildApk")]
+    public static void BuildApk()
     {
-        
-    }
+        var outdir = System.Environment.CurrentDirectory + "/BuildOutPutPath/Android";
+        var outputPath = Path.Combine(outdir, Application.productName + ".apk");
+        //文件夹处理
+        if (!Directory.Exists(outdir)) Directory.CreateDirectory(outdir);
+        if (File.Exists(outputPath)) File.Delete(outputPath);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //开始项目一键打包
+        string[] scenes = new[] { "Assets/Scenes/SampleScene.unity" };
+        UnityEditor.BuildPipeline.BuildPlayer(scenes, outputPath, BuildTarget.Android, BuildOptions.None);
+        if (File.Exists(outputPath))
+        {
+            Debug.Log("Build Success :" + outputPath);
+        }
+        else
+        {
+            Debug.LogException(new Exception("Build Fail! Please Check the log! "));
+        }
     }
 }
